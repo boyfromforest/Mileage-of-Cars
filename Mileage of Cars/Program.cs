62786449061384e3f46ad15769f3db8ef8b89670
Aforvard <62892137+Aforvard@users.noreply.github.com>
2020-04-02 01:13:46 +0300

    Update Program.cs
    New Console.WriteLine

--- Mileage of Cars/Program.cs
+++ Mileage of Cars/Program.cs
@@ -13,7 +13,9 @@ namespace Mileage_of_Cars
             Console.Write("Hi there!\n With this program you can check the exact mileage of your car.");
             Console.WriteLine("Please, enter the year of first registration of your car:");
 
-
+            //
+            Console.WriteLine("Please, check your answer");
+            //
             Console.ReadKey();
         }
     }
